5e59a991c90be270469fb97036c45647e97a864a
moi <randyhari@gmail.com>
2018-04-12 11:06:00 +0200

    ajout de commentaire

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -5,10 +5,13 @@ use rd\skeleton\core\View;
 
 class DefaultController
 {
+    // fichier .css
     private $cssFile = ["default.css", "default_2.css"];
 
     public function defaultAction(){
         $view = new View();
+        
+        //page avec les fichiers .css spécifiques
         echo $view->render("index.php", $this->cssFile);
     }
     
